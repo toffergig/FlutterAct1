91e6c351ddf3659b794abda9ca1621203188e789
Kristoffer John Giganto <kristoffersoncool@gmail.com>
2024-02-21 09:49:39 +0800

    initial commit

--- activity1/lib/main.dart
+++ activity1/lib/main.dart
@@ -7,205 +7,232 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-   @override
+    @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Container(
-          color: const Color(0xFF000668),
-          child: const Align(
-            alignment: Alignment.topCenter,
-            child: Padding(
-              padding: EdgeInsets.all(80.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  CircleAvatar(
-                    radius: 80,
-                    backgroundImage: AssetImage('assets/montgomery.jpg'),
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+          color: const Color.fromARGB(255, 13, 16, 58),
+          child: Stack(
+            children: [
+              const Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(30, 15, 30, 0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Expanded(
-                        child: Text(
-                          "Chris Montgomery",
-                          style: TextStyle(
-                            color: Colors.yellow,
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
+                      SizedBox(height: 60),
+                      CircleAvatar(
+                        radius: 80,
+                        backgroundImage: AssetImage('assets/montgomery.jpg'),
+                      ),
+                      SizedBox(height: 15),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Chris Montgomery",
+                            style: TextStyle(
+                              fontFamily: 'Times New Roman',
+                              color: Colors.yellow,
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
                           ),
-                          textAlign: TextAlign.center,
-                        ),
+                        ],
                       ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(width: 8), // Add space between the texts if needed
-                      Text(
-                        "New York",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 15,
-                          fontWeight: FontWeight.normal,
-                        ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(height: 5.0),
+                          Text(
+                            "New York",
+                            style: TextStyle(
+                              fontFamily: 'Times New Roman',
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                  SizedBox(height: 30),
-                  Row(
-                     mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      SizedBox(width: 8), // Add space between the texts if needed
-                      Icon(
-                        Icons.person_2_outlined,
+                      SizedBox(height: 40),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(width: 8),
+                          Icon(
+                            Icons.person_2_outlined,
+                            color: Colors.yellow,
+                          ),
+                          SizedBox(width: 8),
+                          Text(
+                            "User Details",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Divider(
+                        color: Colors.yellow,
+                        thickness: 1,
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(width: 8),
+                          Icon(
+                            Icons.history_edu_outlined,
+                            color: Colors.yellow,
+                          ),
+                          SizedBox(width: 8),
+                          Text(
+                            "History",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Divider(
                         color: Colors.yellow,
                       ),
-                      SizedBox(width: 8), // Add space between the texts and icon
-                      Text(
-                        "User Details",
-                        style: TextStyle(
-                          color: Colors.yellow,
-                          fontSize: 15,
-                          fontWeight: FontWeight.normal,
-                        ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(width: 8),
+                          Icon(
+                            Icons.book_online_outlined,
+                            color: Colors.yellow,
+                          ),
+                          SizedBox(width: 8),
+                          Text(
+                            "Booking Details",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                  SizedBox(height: 10), // Add space between "User Details" and Divider
-                  Divider(
-                    color: Colors.yellow,
-                  ),
-
-                  SizedBox(height: 20),
-                  Row(
-                     mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      SizedBox(width: 8), // Add space between the texts if needed
-                      Icon(
-                        Icons.history_edu_outlined,
+                      SizedBox(height: 10),
+                      Divider(
                         color: Colors.yellow,
                       ),
-                      SizedBox(width: 8), // Add space between the texts and icon
-                      Text(
-                        "History",
-                        style: TextStyle(
-                          color: Colors.yellow,
-                          fontSize: 15,
-                          fontWeight: FontWeight.normal,
-                        ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(width: 8),
+                          Icon(
+                            Icons.mail_outline,
+                            color: Colors.yellow,
+                          ),
+                          SizedBox(width: 8),
+                          Text(
+                            "Contact Us",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                  SizedBox(height: 10), // Add space between "User Details" and Divider
-                  Divider(
-                    color: Colors.yellow,
-                  ),
-
-                  SizedBox(height: 20),
-                  Row(
-                     mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      SizedBox(width: 8), // Add space between the texts if needed
-                      Icon(
-                        Icons.book_online_outlined,
+                      SizedBox(height: 10),
+                      Divider(
                         color: Colors.yellow,
                       ),
-                      SizedBox(width: 8), // Add space between the texts and icon
-                      Text(
-                        "Booking Details",
-                        style: TextStyle(
-                          color: Colors.yellow,
-                          fontSize: 15,
-                          fontWeight: FontWeight.normal,
-                        ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(width: 8),
+                          Icon(
+                            Icons.share_outlined,
+                            color: Colors.yellow,
+                          ),
+                          SizedBox(width: 8),
+                          Text(
+                            "Share This App",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                  SizedBox(height: 10), // Add space between "User Details" and Divider
-                  Divider(
-                    color: Colors.yellow,
-                  ),  
-
-                  SizedBox(height: 20),
-                  Row(
-                     mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      SizedBox(width: 8), // Add space between the texts if needed
-                      Icon(
-                        Icons.mail_outline,
+                      SizedBox(height: 10),
+                      Divider(
                         color: Colors.yellow,
                       ),
-                      SizedBox(width: 8), // Add space between the texts and icon
-                      Text(
-                        "Contact Us",
-                        style: TextStyle(
-                          color: Colors.yellow,
-                          fontSize: 15,
-                          fontWeight: FontWeight.normal,
-                        ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(width: 8),
+                          Icon(
+                            Icons.help_outline_outlined,
+                            color: Colors.yellow,
+                          ),
+                          SizedBox(width: 8),
+                          Text(
+                            "Help",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                  SizedBox(height: 10), // Add space between "User Details" and Divider
-                  Divider(
-                    color: Colors.yellow,
-                  ),      
-
-                  SizedBox(height: 20),
-                  Row(
-                     mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      SizedBox(width: 8), // Add space between the texts if needed
-                      Icon(
-                        Icons.share_outlined,
+                      SizedBox(height: 10),
+                      Divider(
                         color: Colors.yellow,
                       ),
-                      SizedBox(width: 8), // Add space between the texts and icon
-                      Text(
-                        "Share This App",
-                        style: TextStyle(
-                          color: Colors.yellow,
-                          fontSize: 15,
-                          fontWeight: FontWeight.normal,
-                        ),
+                      SizedBox(height: 40),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(height: 5.0),
+                          Text(
+                            "Sign Out",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                  SizedBox(height: 10), // Add space between "User Details" and Divider
-                  Divider(
-                    color: Colors.yellow,
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                     mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      SizedBox(width: 8), // Add space between the texts if needed
-                      Icon(
-                        Icons.help_outline_outlined,
-                        color: Colors.yellow,
-                      ),
-                      SizedBox(width: 8), // Add space between the texts and icon
-                      Text(
-                        "Help",
-                        style: TextStyle(
-                          color: Colors.yellow,
-                          fontSize: 15,
-                          fontWeight: FontWeight.normal,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10), // Add space between "User Details" and Divider
-                  Divider(
-                    color: Colors.yellow,
-                  ),
-                ],
+                ),
               ),
-            ),
+              Positioned(
+                top: 80,
+                right: 20,
+                child: GestureDetector(
+                  child: const Icon(
+                    Icons.close_sharp,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
